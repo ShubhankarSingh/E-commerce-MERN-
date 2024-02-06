3a49319abffd261a6a5f3941cb64570235b3daf9
Shubhankar Singh <singhshubhankar01@gmail.com>
2024-02-06 23:03:57 +0530

    Create Order Schema to save user orders

---
+++ backend/models/OrderModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+// Define the schema for an order item
+const OrderItemSchema = new mongoose.Schema({
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product', // Reference to the Product model
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+});
+
+// Define the schema for an order
+const OrderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    items: [OrderItemSchema],
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+    orderdate: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Define the Order model using the schema
+const Order = mongoose.model('Order', OrderSchema);
+
+// Export the Order model
+module.exports = Order;
